ddb5293965825ed1cae39cb8a6066d497526b8ea
Parv Shah <parvthecoder@gmail.com>
2023-11-01 16:34:03 +0530

    first commit

--- Array/P1.java
+++ Array/P1.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
-public class P1
-{
+public class P1 {
     public static void main(String[] args) {
         int marks[] = new int[100];
         Scanner sc = new Scanner(System.in);
@@ -11,9 +10,10 @@ public class P1
         System.out.println("Maths = " + marks[0]);
         System.out.println("Physics = " + marks[1]);
         System.out.println("Chemistry = " + marks[2]);
-        
-        int percentage = (marks[0] + marks[1] + marks[2])/3;
+
+        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
         System.out.println("Percentage = " + percentage + "%");
 
+        System.out.println("The length of my array is " + marks.length);
     }
 }
