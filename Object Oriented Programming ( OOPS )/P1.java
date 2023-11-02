61615a4d55015bc69b0086c39996ef0cbbcee5c7
Parv Shah <parvthecoder@gmail.com>
2023-11-02 20:22:39 +0530

    Introduction to OOPS

---
+++ Object Oriented Programming ( OOPS )/P1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class P1 {
+
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.setcolor("RED");
+        System.out.println(p1.getcolor());
+        p1.settip(5);
+        System.out.println(p1.gettip());
+    }
+
+}
+
+class Pen {
+    private String color;
+    private int tip;
+
+    String getcolor() {
+        return this.color;
+    }
+
+    int gettip() {
+        return this.tip;
+    }
+
+    void setcolor(String newcolor) {
+        this.color = newcolor;
+    }
+
+    void settip(int newtip) {
+        this.tip = newtip;
+    }
+}
