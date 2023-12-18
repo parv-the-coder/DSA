4aeb06eeeb0e322bc2152838bba9ae5d820ed564
Parv Shah <parvthecoder@gmail.com>
2023-12-18 22:00:11 +0530

    Compareto

--- Strings/P7.java
+++ Strings/P7.java
@@ -1,3 +1,12 @@
 public class P7 {
-    
+    public static void main(String[] args) {
+        String fruits[] = { "apple", "banana", "mango" };
+        String largest = fruits[0];
+        for (int i = 0; i < fruits.length; i++) {
+            if (largest.compareToIgnoreCase(fruits[i]) < 0) {
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
+    }
 }
