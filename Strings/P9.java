1363d40c372ab211da75897404101441860cc2a0
Parv Shah <parvthecoder@gmail.com>
2023-12-18 22:31:33 +0530

    Camel Case

---
+++ Strings/P9.java
@@ -0,0 +1,24 @@
+
+public class P9 {
+    public static String CamelCase(String str) {
+        StringBuilder sb = new StringBuilder();
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "hi, i'm Parv king.  ";
+        String str2 = "hihi WORLD";
+        System.out.println(CamelCase(str2));
+    }
+}
