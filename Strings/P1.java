085418a7fbaa10e21c1584b28490ff5c10941cc4
Parv Shah <parvthecoder@gmail.com>
2023-12-18 17:27:29 +0530

    String Basics

---
+++ Strings/P1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P1 {
+    public static void main(String[] args) {
+        // String str = "Parv";
+        // String str2 = new String("Kamlesh");
+        Scanner sc = new Scanner(System.in);
+        // String code = sc.next();
+        // System.out.println(code);
+        // String email = sc.nextLine();
+        // System.out.println(email);
+        String firstName = "Parv";
+        String lastName = "King";
+        String FullName = firstName + " " + lastName;
+        System.out.println(FullName);
+        System.out.println(FullName.length());
+        System.out.println(FullName.charAt(0));
+
+    }
+}
