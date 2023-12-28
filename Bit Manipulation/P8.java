a71e040299183eca45f222c48cbc9400f3b55467
Parv Shah <parvthecoder@gmail.com>
2023-12-28 12:02:44 +0530

    Count Set Bits

---
+++ Bit Manipulation/P8.java
@@ -0,0 +1,16 @@
+public class P8 {
+    public static int coutsetbits(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(coutsetbits(15));
+    }
+}
