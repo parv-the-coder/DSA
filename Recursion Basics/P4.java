b11fa504c052e187953cf11551e8197a1f2955bb
Parv Shah <parvthecoder@gmail.com>
2024-01-10 15:38:30 +0530

    Sum of N natural numbers

---
+++ Recursion Basics/P4.java
@@ -0,0 +1,13 @@
+public class P4 {
+    public static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(sum(n));
+    }
+}
