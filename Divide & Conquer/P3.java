17b1eb0ec84dc9acabda0c9edbc674dee3d447cc
Parv Shah <parvthecoder@gmail.com>
2024-02-02 17:02:38 +0530

    Search in Rotated Sorted Array

---
+++ Divide & Conquer/P3.java
@@ -0,0 +1,32 @@
+public class P3 {
+    public static int search(int arr[], int target, int si, int ei) {
+        if (si > ei) {
+            return -1;
+        }
+
+        int mid = si + (ei - si) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        if (arr[si] <= arr[mid]) {
+            if (arr[si] <= target && target <= arr[mid]) {
+                return search(arr, target, si, mid - 1);
+            } else {
+                return search(arr, target, mid + 1, ei);
+            }
+        } else {
+            if (arr[mid] <= target && target <= arr[ei]) {
+                return search(arr, target, mid + 1, ei);
+            } else {
+                return search(arr, target, si, mid - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 5, 6, 7, 0, 1, 2, 3 };
+        System.out.println(search(arr, 0, 0, arr.length - 1));
+    }
+}
