134f5296f0098129bf1adc64990edf35b0c87894
Parv Shah <parvthecoder@gmail.com>
2024-02-26 11:03:27 +0530

    initial commit

---
+++ Backtracking/P1.java
@@ -0,0 +1,26 @@
+public class P1 {
+    public static void changeArr(int arr[], int i, int value) {
+        // Base Case
+        if (i == arr.length) {
+            printArr(arr);
+            return;
+        }
+        // recursion
+        arr[i] = value;
+        changeArr(arr, i + 1, value + 1);
+        arr[i] = value - 2;
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        changeArr(arr, 0, 1);
+        printArr(arr);
+    }
+}
