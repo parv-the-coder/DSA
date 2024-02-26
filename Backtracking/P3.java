d5468c0f3f1f04392a88197172e3aaea08286b81
Parv Shah <parvthecoder@gmail.com>
2024-02-26 11:41:54 +0530

    Permutaion

---
+++ Backtracking/P3.java
@@ -0,0 +1,20 @@
+public class P3 {
+    public static void findPermutation(String str, String ans) {
+        //base 
+        if (str.length()==0) {
+            System.out.println(ans);
+            return;
+        }
+        //recursion
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            String newstr = str.substring(0, i) + str.substring(i + 1);
+            findPermutation(newstr, ans + curr);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        findPermutation(str, "");
+    }
+}
