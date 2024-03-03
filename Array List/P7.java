6628fc355d1746f2d1af694603e40ae17ac42f96
Parv Shah <parvthecoder@gmail.com>
2024-03-03 18:05:07 +0530

    Multi-Dimensional ArrayList

---
+++ Array List/P7.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class P7 {
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> list3 = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            list1.add(i * 1); // 1 2 3 4 5
+            list2.add(i * 2); // 2 4 6 8 10
+            list3.add(i * 3); // 3 6 9 12 15
+        }
+
+        mainList.add(list1);
+        mainList.add(list2);
+        mainList.add(list3);
+
+        System.err.println(mainList);
+
+        for (int i = 0; i < mainList.size(); i++) {
+            ArrayList<Integer> currentList = mainList.get(i);
+            for (int j = 0; j < currentList.size(); j++) {
+                System.out.print(currentList.get(j) + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
