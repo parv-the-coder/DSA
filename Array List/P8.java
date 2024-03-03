8e572d23295c8aff663e42affbda0c2ec84de43e
Parv Shah <parvthecoder@gmail.com>
2024-03-03 18:36:52 +0530

    Container Using Brute Force

---
+++ Array List/P8.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class P8 {
+    public static int Container(ArrayList<Integer> height) {
+        int maxWater = 0;
+        // brute Force
+        for (int i = 0; i < height.size(); i++) {
+            for (int j = i + 1; j < height.size(); j++) {
+                int ht = Math.min(height.get(i), height.get(j));
+                int width = j - i;
+                int currWater = ht * width;
+                maxWater = Math.max(maxWater, currWater);
+            }
+        }
+        return maxWater;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+        System.out.println(Container(height));
+    }
+}
