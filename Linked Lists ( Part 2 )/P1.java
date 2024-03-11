2a7a29bc6052ece3308a68e7fe41176517e4fc12
Parv Shah <parvthecoder@gmail.com>
2024-03-11 12:38:03 +0530

    Detecting Cycle in LL

---
+++ Linked Lists ( Part 2 )/P1.java
@@ -0,0 +1,51 @@
+public class P1 {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+
+    public void printLinkedList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+
+    public static boolean isCycle() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        P1 ll = new P1();
+        head = new Node(0);
+        head.next = new Node(1);
+        head.next.next = new Node(2);
+        head.next.next.next = new Node(3);
+        // head.next.next.next.next = head;
+        System.out.println(isCycle());
+    }
+}
