a8ca31f1a4a429278e327b1c875ea702f0e82395
Parv Shah <parvthecoder@gmail.com>
2024-03-14 22:44:32 +0530

    Doubly LinkedList

---
+++ Linked Lists ( Part 2 )/P6.java
@@ -0,0 +1,77 @@
+import org.w3c.dom.Node;
+
+public class P6 {
+    public static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+
+        // step-1 create new Node
+        Node newNode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    public void printLinkedList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public int removeFirst() {
+        if (head == null) {
+            System.out.println("Dll is empty");
+            return Integer.MIN_VALUE;
+        }
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
+    public static void main(String[] args) {
+        P6 dll = new P6();
+        dll.addFirst(4);
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.addFirst(0);
+        dll.printLinkedList();
+        System.out.println(dll.size);
+        System.out.println(dll.removeFirst());
+        dll.printLinkedList();
+        System.out.println(dll.size);
+
+    }
+}
